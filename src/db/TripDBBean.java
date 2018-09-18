d215b3e2991c320f1de8dcddbac2bd95786c7c79
Lazy Talysa <42195207+Talysa7@users.noreply.github.com>
2018-09-18 15:57:53 +0900

    start work

--- src/db/TripDBBean.java
+++ src/db/TripDBBean.java
@@ -11,4 +11,8 @@ import org.apache.ibatis.session.SqlSession;
 public class TripDBBean {
 	SqlSession session;
 	
+	public TripDataBean getTrip() {
+		TripDataBean tripDto=new TripDataBean();
+		return tripDto;
+	}
 }
