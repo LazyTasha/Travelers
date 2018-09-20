a03fd8b37d262f2f62a622217f4c061c35263f41
Talysa Lee <hisia@naver.com>
2018-09-20 16:44:38 +0900

    Second day merge

--- src/db/TripDBBean.java
+++ src/db/TripDBBean.java
@@ -1,14 +1,17 @@
-/*
- * !!! 주의사항 !!!
- * 전부 void, 인자 없음으로 메소드 이름만 정의해둔 상태.
- * 메소드마다 리턴 값, 인자 알아서 채워 넣을 것.
- */
-
 package db;
 
 import org.apache.ibatis.session.SqlSession;
 
+import bean.SqlMapClient;
+
 public class TripDBBean {
-	SqlSession session;
+	SqlSession session=SqlMapClient.getSession();
 	
+	public TripDataBean getTrip(int tb_no) {
+		TripDataBean tripDto=session.selectOne("db.getTrip", tb_no);
+		return tripDto;
+	}
+	public void addViewCount(int tb_no) {
+		session.update("db.addViewCount", tb_no);
+	}
 }
