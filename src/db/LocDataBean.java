f4ca68da6bdf4a825b0740beb4ae8ca291c003a9
ssr03 <43363127+ssr03@users.noreply.github.com>
2018-10-11 20:44:43 +0900

    writeform 작성시 좌표값 추가
    writeform 작성시 좌표값 추가 및 장소 저장

--- src/db/LocDataBean.java
+++ src/db/LocDataBean.java
@@ -3,6 +3,12 @@ package db;
 public class LocDataBean {
 	private int num;
 	private int tripId;
+	private int country_id;
+	private String country_code;
+	private double coord_long;
+	private double coord_lat;
+	private int coord_order;
+	private int coord_id;
 	public int getNum() {
 		return num;
 	}
@@ -14,5 +20,41 @@ public class LocDataBean {
 	}
 	public void setTripId(int tripId) {
 		this.tripId = tripId;
+	}
+	public int getCountry_id() {
+		return country_id;
+	}
+	public void setCountry_id(int country_id) {
+		this.country_id = country_id;
+	}
+	public String getCountry_code() {
+		return country_code;
+	}
+	public void setCountry_code(String country_code) {
+		this.country_code = country_code;
+	}
+	public double getCoord_long() {
+		return coord_long;
+	}
+	public void setCoord_long(double coord_long) {
+		this.coord_long = coord_long;
+	}
+	public double getCoord_lat() {
+		return coord_lat;
+	}
+	public void setCoord_lat(double coord_lat) {
+		this.coord_lat = coord_lat;
+	}
+	public int getCoord_order() {
+		return coord_order;
+	}
+	public void setCoord_order(int coord_order) {
+		this.coord_order = coord_order;
+	}
+	public int getCoord_id() {
+		return coord_id;
+	}
+	public void setCoord_id(int coord_id) {
+		this.coord_id = coord_id;
 	}
 }
